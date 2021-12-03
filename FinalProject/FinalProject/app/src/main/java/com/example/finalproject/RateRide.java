00760c1e698b83952fa5a8cdff3a26898ad84e2e
LisaHanna13 <60234735+LisaHanna13@users.noreply.github.com>
2021-12-02 19:14:45 -0500

    First part of project done

---
+++ FinalProject/FinalProject/app/src/main/java/com/example/finalproject/RateRide.java
@@ -0,0 +1,14 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RateRide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_ride);
+    }
+}
